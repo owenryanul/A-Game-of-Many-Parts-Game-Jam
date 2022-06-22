1ea499656f6e1936b6bcc466b60132b24e167211
Owen Ryan <owenryanul@gmail.com>
2022-06-22 20:23:57 +0100

    <Battle Enemy Animations and Encapsulation>

--- Assets/Oryan_Part/Battle Enemy/Oryan_BasicBattleEnemy.cs
+++ Assets/Oryan_Part/Battle Enemy/Oryan_BasicBattleEnemy.cs
@@ -7,7 +7,6 @@ public class Oryan_BasicBattleEnemy : Oryan_TurnTaker, Oryan_ProjectileParent
 {
     public GameObject projectile;
     public GameObject parryProjectile;
-    public bool doShoot = true;
 
     public int maxHp;
     private int currentHp;
@@ -15,22 +14,38 @@ public class Oryan_BasicBattleEnemy : Oryan_TurnTaker, Oryan_ProjectileParent
     public List<Oryan_Element> vulnerablies;
     public List<Oryan_Element> immunities;
 
+    private int activeChildProjectiles;
+    private bool isPlayingAttackAnimation;
+    private bool isPlayingReadyAnimation;
+
     private bool destroyNextUpdate;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         currentHp = maxHp;
         this.gameObject.transform.Find("Sprite").Find("Enemy Canvas").Find("HP Text").GetComponent<TextMeshProUGUI>().text = "" + this.currentHp;
+        activeChildProjectiles = 0;
         destroyNextUpdate = false;
+
+        isPlayingReadyAnimation = false;
+        isPlayingAttackAnimation = false;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         if(destroyNextUpdate)
         {
             kill();
+        }
+
+        if(activeChildProjectiles == 0 && !isPlayingReadyAnimation && !isPlayingAttackAnimation)
+        {
+            if (base.isTheirTurn)
+            {
+                base.endTheirTurn();
+            }
         }
     }
 
@@ -44,22 +59,30 @@ public class Oryan_BasicBattleEnemy : Oryan_TurnTaker, Oryan_ProjectileParent
         Debug.Log("OnTheirTurnStarted " + this.gameObject.name);
         base.onTheirTurnStart();
 
-        if (doShoot)
+        isPlayingReadyAnimation = true;
+        this.gameObject.GetComponent<Animator>().SetTrigger("Ready");
+    }
+
+    public virtual void makeAttack()
+    {
+        this.isPlayingAttackAnimation = true;
+        this.gameObject.GetComponent<Animator>().SetTrigger("Attack");
+
+        if (Random.Range(0, 2) == 0)
         {
-            if(Random.Range(0, 2) == 0)
-            {
-                Vector3 directionTowardsPlayer = GameObject.FindGameObjectWithTag("base_player").transform.position - this.gameObject.transform.position;
-                GameObject bolt = Instantiate(projectile, this.gameObject.transform.position, projectile.transform.rotation);
-                bolt.GetComponent<Oryan_EnemyProjectile>().setProjectileParent(this);
-                bolt.GetComponent<Oryan_EnemyProjectile>().fireInDirection(directionTowardsPlayer);
-            }
-            else
-            {
-                Vector3 directionTowardsPlayer = GameObject.FindGameObjectWithTag("base_player").transform.position - this.gameObject.transform.position;
-                GameObject bolt = Instantiate(parryProjectile, this.gameObject.transform.position, projectile.transform.rotation);
-                bolt.GetComponent<Oryan_EnemyProjectile>().setProjectileParent(this);
-                bolt.GetComponent<Oryan_EnemyProjectile>().fireInDirection(directionTowardsPlayer);
-            }
+            Vector3 directionTowardsPlayer = GameObject.FindGameObjectWithTag("base_player").transform.position - this.gameObject.transform.position;
+            GameObject bolt = Instantiate(projectile, this.gameObject.transform.position, projectile.transform.rotation);
+            bolt.GetComponent<Oryan_EnemyProjectile>().setProjectileParent(this);
+            bolt.GetComponent<Oryan_EnemyProjectile>().fireInDirection(directionTowardsPlayer);
+            activeChildProjectiles++;
+        }
+        else
+        {
+            Vector3 directionTowardsPlayer = GameObject.FindGameObjectWithTag("base_player").transform.position - this.gameObject.transform.position;
+            GameObject bolt = Instantiate(parryProjectile, this.gameObject.transform.position, projectile.transform.rotation);
+            bolt.GetComponent<Oryan_EnemyProjectile>().setProjectileParent(this);
+            bolt.GetComponent<Oryan_EnemyProjectile>().fireInDirection(directionTowardsPlayer);
+            activeChildProjectiles++;
         }
     }
 
@@ -88,8 +111,6 @@ public class Oryan_BasicBattleEnemy : Oryan_TurnTaker, Oryan_ProjectileParent
                 immunityTriggered = true;
             }
         }
-
-
 
 
         currentHp -= totalDamage;
@@ -130,14 +151,22 @@ public class Oryan_BasicBattleEnemy : Oryan_TurnTaker, Oryan_ProjectileParent
 
     public void childProjectileDestroyed()
     {
-        if(base.isTheirTurn)
-        {
-            base.endTheirTurn();
-        }
+        activeChildProjectiles--;
     }
 
     public override void markForDeletion()
     {
         destroyNextUpdate = true;
+    }
+
+    public void OnReadyAnimationDone()
+    {
+        isPlayingReadyAnimation = false;
+        makeAttack();
+    }
+
+    public void OnAttackAnimationDone()
+    {
+        isPlayingAttackAnimation = false;
     }
 }
