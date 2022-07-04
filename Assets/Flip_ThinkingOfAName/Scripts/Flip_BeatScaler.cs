033d3f2547595c219408f726e3b92f9e0494677e
Philip-Waldron <philip_waldron@yahoo.com>
2022-07-04 14:29:37 +0100

    Add BeatScaler script

---
+++ Assets/Flip_ThinkingOfAName/Scripts/Flip_BeatScaler.cs
@@ -0,0 +1,29 @@
+using SynchronizerData;
+using UnityEngine;
+
+public class Flip_BeatScaler : MonoBehaviour
+{
+    public BeatObserver BeatObserver;
+    public BeatType BeatType;
+    public Transform TargetToScale;
+    public Vector3 NeutralScale;
+    public Vector3 BeatScale;
+    public float TimeToResetScale;
+
+    private float _elapsedTime;
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if ((BeatObserver.beatMask & BeatType) == BeatType)
+        {
+            TargetToScale.localScale = BeatScale;
+            _elapsedTime = 0;
+        }
+        else
+        {
+            TargetToScale.localScale = Vector3.Lerp(BeatScale, NeutralScale, _elapsedTime / TimeToResetScale);
+        }
+    }
+}
