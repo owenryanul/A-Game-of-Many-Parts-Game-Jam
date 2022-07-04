533e29b7b154fb34e833885c720b705115f14935
Philip-Waldron <philip_waldron@yahoo.com>
2022-07-04 16:13:26 +0100

    Add credits

--- Assets/Flip_ThinkingOfAName/Scripts/BeatSynchronizer/Flip_SynchronizerData.cs
+++ Assets/Flip_ThinkingOfAName/Scripts/BeatSynchronizer/Flip_SynchronizerData.cs
@@ -1,4 +1,22 @@
-﻿namespace Flip_SynchronizerData
+﻿// License
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Christian Floisand
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+namespace Flip_SynchronizerData
 {
     // BeatValue determines which beat to synchronize with, and is specified for each BeatCounter instance.
     // (A sequence of beat values are specified for PatternCounter instances).
