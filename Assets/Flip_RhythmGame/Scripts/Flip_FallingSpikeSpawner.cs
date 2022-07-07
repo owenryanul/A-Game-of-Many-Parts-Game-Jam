4f57cb52bf8b8b13b9dbd9a8d549f893e3f9216f
Philip-Waldron <philip_waldron@yahoo.com>
2022-07-07 15:48:00 +0100

    Remove old random call on falling spike

--- Assets/Flip_RhythmGame/Scripts/Flip_FallingSpikeSpawner.cs
+++ Assets/Flip_RhythmGame/Scripts/Flip_FallingSpikeSpawner.cs
@@ -8,7 +8,6 @@ public class Flip_FallingSpikeSpawner : MonoBehaviour, Flip_ISpawnable
 
     public void Spawn()
     {
-        Vector2 position = new Vector2(Random.Range(SpawnPositionMin.x, SpawnPositionMax.x), Random.Range(SpawnPositionMin.y, SpawnPositionMax.y));
         Flip_FallingSpike spike = Instantiate(FallingSpike, position, Quaternion.Euler(0, 0, 180)).GetComponent<Flip_FallingSpike>();
         spike.StartPosition = new Vector2(Player.position.x, 4.9f);
         spike.EndPosition = new Vector2(Player.position.x, 4.9f);
