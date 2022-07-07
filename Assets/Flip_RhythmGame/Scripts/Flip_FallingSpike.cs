3628963a8c392a627e027f130470038c4d97e559
Philip-Waldron <philip_waldron@yahoo.com>
2022-07-07 16:06:16 +0100

    Revert to using random positions for falling spikes

--- Assets/Flip_RhythmGame/Scripts/Flip_FallingSpike.cs
+++ Assets/Flip_RhythmGame/Scripts/Flip_FallingSpike.cs
@@ -10,8 +10,6 @@ public class Flip_FallingSpike : MonoBehaviour
     public AnimationCurve XPositionAnimationCurve;
     public AnimationCurve YPositionAnimationCurve;
 
-    private const float spawnDelay = 1;
-
     public void Spawn()
     {
         SpikeRigidbody.simulated = true;
@@ -24,17 +22,9 @@ public class Flip_FallingSpike : MonoBehaviour
     {
         float timeElapsed = 0;
 
-        while (timeElapsed < spawnDelay)
+        while (timeElapsed < 2)
         {
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        timeElapsed -= spawnDelay;
-
-        while (timeElapsed < 1)
-        {
-            var animationCurveValues = new Vector2(XPositionAnimationCurve.Evaluate(timeElapsed) * 2, YPositionAnimationCurve.Evaluate(timeElapsed) * 2);
+            var animationCurveValues = new Vector2(XPositionAnimationCurve.Evaluate(timeElapsed), YPositionAnimationCurve.Evaluate(timeElapsed));
             transform.position = new Vector2(Mathf.Lerp(StartPosition.x, EndPosition.x, animationCurveValues.x), Mathf.Lerp(StartPosition.y, EndPosition.y, animationCurveValues.y));
 
             timeElapsed += Time.deltaTime;
