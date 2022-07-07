4c5be5ae6326eab2d64136d999b96576ef7441b5
Philip-Waldron <philip_waldron@yahoo.com>
2022-07-07 15:49:06 +0100

    Fix instantiate using wrong position in falling spike spawner

--- Assets/Flip_RhythmGame/Scripts/Flip_FallingSpikeSpawner.cs
+++ Assets/Flip_RhythmGame/Scripts/Flip_FallingSpikeSpawner.cs
@@ -8,7 +8,7 @@ public class Flip_FallingSpikeSpawner : MonoBehaviour, Flip_ISpawnable
 
     public void Spawn()
     {
-        Flip_FallingSpike spike = Instantiate(FallingSpike, position, Quaternion.Euler(0, 0, 180)).GetComponent<Flip_FallingSpike>();
+        Flip_FallingSpike spike = Instantiate(FallingSpike, new Vector2(Player.position.x, 4.9f), Quaternion.Euler(0, 0, 180)).GetComponent<Flip_FallingSpike>();
         spike.StartPosition = new Vector2(Player.position.x, 4.9f);
         spike.EndPosition = new Vector2(Player.position.x, 4.9f);
         spike.Spawn();
